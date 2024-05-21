d1e031f6a6a970ccb30eaf016d75e05114172052
MudKip-lv1 <aaron.ventura0321@gmail.com>
2024-05-21 22:00:16 +0900

    add_string_file

---
+++ works/string_basic.py
@@ -0,0 +1,19 @@
+#文字列(String)の基本操作
+
+print("Hello World") 
+print (1)
+
+print ("Hello" + "World") #文字列の連結
+print ("Hello" * 3) #文字列の繰り返し
+# print ("Hello" + 1) #エラー
+
+print ("""
+       Hello World!!
+       Hello Python!!
+       Hello Everyone!!""") #複数行の文字列
+
+print ("hello \t world") #タブ
+print("backslash n: \\n") #バックスラッシュ
+
+print("hello" + "world") #文字列の連結
+print("hello" + " " + "world") #文字列の連結 + 空白
