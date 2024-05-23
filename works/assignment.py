2976b6464bb2443d591d29d48bb01abd735c96d9
MudKip-lv1 <aaron.ventura0321@gmail.com>
2024-05-23 12:56:05 +0900

    add_py_file

---
+++ works/assignment.py
@@ -0,0 +1,18 @@
+# #変数へ代入：assignment
+
+# # a = "Hello"
+# # b = "World"
+# # print(a + b)
+
+#formatメソッド
+prefix = "Hello"
+suffix = "World"
+
+print("{},{}".format(suffix,prefix))
+
+
+#fstringについて
+a = "Hello"
+b = "World"
+
+print(f"{a},{b}")
