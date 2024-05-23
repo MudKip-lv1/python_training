629553a87fad4b24498d30eadcf2f3d5510ceab4
MudKip-lv1 <aaron.ventura0321@gmail.com>
2024-05-23 16:41:29 +0900

    fix_commit

---
+++ works/bool.py
@@ -0,0 +1,14 @@
+#boolean型: True, False
+print(True)
+print(False)
+
+balance = 1000
+withdrawal = 500
+
+print(balance > withdrawal)
+
+print(5 > 3)
+print(2.0 <= 2.0)
+
+print(5  == 5)
+print(5 != 5)
